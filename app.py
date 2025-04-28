cc370c5e92915f4e446531591bdf9b67e0fc5a9a
Roshan Kumar Sahu <igniter909505@gmail.com>
2025-04-28 16:06:52 +0530

    updated

--- app.py
+++ app.py
@@ -4,15 +4,28 @@ from face_utils import base64_to_image, generate_embedding, compare_embeddings
 import os
 from flask_cors import CORS
 from bson import ObjectId
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 CORS(app, resources={
     r"/*": {
         "origins": ["*"],
         "methods": ["GET", "POST", "OPTIONS"],
-        "allow_headers": ["Content-Type"]
+        "allow_headers": ["Content-Type", "Accept"],
+        "supports_credentials": True
     }
 })
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Accept')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
 
 @app.route('/')
 def index():
